314e63904cade128b114b1321d98397cc8f062bf
Fanchennan <444852554@qq.com>
2018-10-10 21:58:29 -0700

    new

---
+++ docs/TEST-19/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+
+  module: {
+     rules: [
+       {
+         test: /\.scss$/,
+         use: [
+           'style-loader',
+           'css-loader',
+           'sass-loader'
+         ]
+       },
+       {
+//         // 对非文本文件采用 file-loader 加载
+         test: /\.(gif|png|jpe?g|eot|woff|ttf|svg|pdf)$/,
+          use: ['file-loader']
+       },
+       {
+          test: /\.html$/,
+          loader: "raw-loader" // loaders: ['raw-loader'] is also perfectly acceptable.
+       }
+
+
+     ]
+   },
+
+   plugins:[ new ExtractTextPlugin('[name].css')],
+   devServer: {
+     contentBase: './dist'
+     // hot: true,   //用ExtractTextPlugin插件时用热更新会报错
+   }
+};
+
+
+/*
+scss 的常见写法
+https://webpack.docschina.org/plugins/extract-text-webpack-plugin/
+安装 npm install extract-text-webpack-plugin@next --save-dev
+
+
+*/
